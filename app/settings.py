550b4893f1ebd74ad9daf46c37db5001a5369b2c
genisis847 <62677013+genisis847@users.noreply.github.com>
2020-03-26 14:56:13 -0500

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -146,8 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
